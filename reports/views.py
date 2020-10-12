221e72d039e2315f6242ec0f26668b6a0836f329
zapper112 <satyarth.shankar@gmail.com>
2020-10-12 13:03:48 +0530

    displaying student wise, subject wise total hours and hourly compensation

--- reports/views.py
+++ reports/views.py
@@ -6,14 +6,16 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def dashboard(request):
     classes = Class.objects.filter(
-        date__gte='2020-10-01'
+        date__gte='2020-10-01',
+        date__lte='2020-10-31'
     )
-    total, hours = get_totals(classes)
+    total, hours, hourly_average = get_totals(classes)
     student_wise = get_student_wise(classes)
     subject_wise = get_subject_wise(classes)
     return render(request, 'reports/dashboard.html', {'classes': classes,
                                                       'total': total,
                                                       'hours': hours,
+                                                      'hourly_average' : hourly_average,
                                                       'student_wise': student_wise,
                                                       'subject_wise' : subject_wise})
 
@@ -24,16 +26,19 @@ def get_totals(classes):
     for c in classes:
         total += c.payout
         hours += c.duration
-    return total, hours
+    return total, hours, int(total/hours)
 
 
 def get_student_wise(classes):
     s_wise = {}
     for c in classes:
         if c.student in s_wise:
-            s_wise[c.student] += c.payout
+            s_wise[c.student][0] += c.payout
+            s_wise[c.student][1] += c.duration
         else:
-            s_wise[c.student] = c.payout
+            s_wise[c.student] = []
+            s_wise[c.student].append(c.payout)
+            s_wise[c.student].append(c.duration)
     s_wise = {k: v for k, v in sorted(s_wise.items(), key=lambda item: item[1], reverse=True)}
     return s_wise
 
@@ -42,8 +47,11 @@ def get_subject_wise(classes):
     s_wise = {}
     for c in classes:
         if c.subject in s_wise:
-            s_wise[c.subject] += c.payout
+            s_wise[c.subject][0] += c.payout
+            s_wise[c.subject][1] += c.duration
         else:
-            s_wise[c.subject] = c.payout
+            s_wise[c.subject] = []
+            s_wise[c.subject].append(c.payout)
+            s_wise[c.subject].append(c.duration)
     s_wise = {k: v for k, v in sorted(s_wise.items(), key=lambda item: item[1], reverse=True)}
     return s_wise
