22b299678213962c9747f230ebec6cd96a422788
zapper112 <satyarth.shankar@gmail.com>
2020-10-12 20:20:56 +0530

    chart fixes : passing correct context to js, inclusion of student pie charts

--- charts/views.py
+++ charts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from reports.models import Class
+import reports.views as rviews
 from json import dumps
 
 
@@ -10,11 +11,14 @@ def home(request):
     )
     day_totals = get_day_totals(classes)
     charts_date, charts_day_totals, charts_cum_totals = get_for_chart(day_totals)
+    charts_students, charts_stu_totals = get_student_wise(classes)
     context = {
         "day_totals": day_totals,
         "charts_date": charts_date,
         "charts_day_totals" : charts_day_totals,
-        "charts_cum_totals" : charts_cum_totals
+        "charts_cum_totals" : charts_cum_totals,
+        "students": charts_students,
+        "chart_student_totals" : charts_stu_totals
     }
     return render(request, 'charts/home.html', context)
 
@@ -39,8 +43,19 @@ def get_for_chart(day_totals):
     d_t = []
     d_c = []
     for key in day_totals:
-        dates.append(int(key.strftime("%d")))
+        dates.append(f"{key.strftime('%d')} {key.strftime('%b')}")
         d_t.append(day_totals[key][0])
         d_c.append(day_totals[key][1])
 
     return dates, d_t, d_c
+
+
+def get_student_wise(classes):
+    s_wise = {}
+    for c in classes:
+        if c.student in s_wise:
+            s_wise[c.student] += c.payout
+        else:
+            s_wise[c.student] = c.payout
+    s_wise = {k: v for k, v in sorted(s_wise.items(), key=lambda item: item[1], reverse=True)}
+    return list(s_wise.keys()), list(s_wise.values())
