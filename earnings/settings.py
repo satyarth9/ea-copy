446db37add55d1d56283c276b49cff7cb7e0abfe
zapper112 <satyarth.shankar@gmail.com>
2020-12-25 18:56:26 +0530

    favicon added

--- earnings/settings.py
+++ earnings/settings.py
@@ -124,11 +124,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
 
 # Add these new lines
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'earnings/static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'home/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
