e637e77291391f1d76b31c65d34f9996c5f9bc2f
zapper112 <satyarth.shankar@gmail.com>
2022-10-07 20:53:49 +0530

    set debug to false

--- earnings/settings.py
+++ earnings/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&@b_vk3r(_soa3fxsz06tcw7qiq2mx+aqtbejvsouj6&328)3o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
